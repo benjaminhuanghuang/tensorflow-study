331e592df5134d9110765ba6cf5827f2a81cf537
Benjamin Huang <bhuang@rms.com>
2018-07-20 19:33:13 -0700

    linear basic

--- tf_models/tf_linear_basic.py
+++ tf_models/tf_linear_basic.py
@@ -5,8 +5,8 @@ x_data = np.random.rand(100)
 y_data = x_data * 0.1 + 0.2
 
 # create a linear model
-w = tf.Variable(0)
-b = tf.Variable(0)
+w = tf.Variable(0.)
+b = tf.Variable(0.)
 
 y_predict = w * x_data + b
 
@@ -17,3 +17,16 @@ loss = tf.reduce_mean(tf.square(y_predict - y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.2)
 
 train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for step in range(201):
+        sess.run(train)
+        if step %20 == 0:
+            print (step, sess.run([w, b]))
+            
+                        
+
+
